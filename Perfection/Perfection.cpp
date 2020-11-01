f6b6021b82454d6147c142c32f1ce1fb644a1d7a
Viktor Béres <beresviktor07@gmail.com>
2020-11-01 14:23:18 +0100

    Final solution new

--- Perfection/Perfection.cpp
+++ Perfection/Perfection.cpp
@@ -5,7 +5,7 @@
         int a = 0, sum = 0;
         do {
             std::cin >> a;
-            if (a == 0) { break; }; //probably not the best solution for the problem, that it writes perfect for 0 in the end
+            if (a == 0) { break; };
             sum = 0;
             for (int i = 1; i <= sqrt(a);i++)
             {
@@ -13,9 +13,7 @@
             }
             sum -= a;
 			std::cout << a;
-            std::cout << sum < a ? " DEFICIENT\n" : sum == a ? " PERFECT\n" : " ABUNDANT\n";         
+            std::cout << ((sum < a) ? " DEFICIENT\n" : (sum == a) ? " PERFECT\n" : " ABUNDANT\n");         
         } while (a != 0);
         return 0;
     }
-    //pukk
-    //puk2
